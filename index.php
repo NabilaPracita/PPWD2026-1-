2b3c83a6837fbbb01c216000fa3de7cdee85f10e
NabilaPracita <h1101251040@student.untan.ac.id>
2026-09-04 22:58:41 +0700

    sudah ditambahkan

--- index.php
+++ index.php
@@ -1,17 +1,65 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="id">
 <head>
-    <link rel="stylesheet" href="cssnih/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Praktikum Pemrograman Web Dasar</title>
+    <title>Pengenalan Nabila</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php
-    echo "Hello, Nabila paling cantik!";
-    ?>
-</body>
 
+    <div class="container">
+ 
+        <!-- Navigasi -->
+        <nav>
+            <h3>NABILA</h3>
+            <div class="menu">
+                <!-- belum dibuat yah -->
+                <a href="#">Home</a> 
+                <a href="#">Tentang</a>
+                <a href="#">Hobi</a>
+                <a href="#">Kontak</a>
+            </div>
+        </nav>
+
+        <section class="intro">
+            <!-- Intro -->
+            <p class="welcome">HALO, PERKENALKAN!</p>
+
+            <h1>Hai, Saya <span>Nabila</span></h1>
+
+            <p class="deskripsi">
+                Saya adalah seorang mahasiswa semester 3
+                Program Studi Sistem Informasi Fakultas MIPA 
+                Universitas Tanjungpura. 
+            </p>
+
+            <div class="button">
+                <a href="#">Tentang Saya</a>
+                <a href="#" class="outline">Lihat Hobi</a>
+            </div>
+
+        </section>
+
+        <!-- Foto Nabila -->
+        <div class="foto">
+
+            <div class="card">
+                <img src="fotoku2.jpeg" alt="Foto 1">
+            </div>
+
+            <div class="card utama">
+                <img src="fotoku7.jpeg" alt="Foto Nabila">
+            </div>
+
+            <div class="card">
+                <img src="fotoku8.jpeg" alt="Foto 3">
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
 </html>
